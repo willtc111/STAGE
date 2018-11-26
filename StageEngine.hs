344edfeab1dc0f2ab8f2ebe8bf238d60e90056d2
William Thomas Carver <wtc9653@cs.rit.edu>
2018-11-26 18:12:57 -0500

    Fixed a bunch of problems in StageEngine

--- StageEngine.hs
+++ StageEngine.hs
@@ -1,40 +1,56 @@
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import Data.Maybe
+import System.Environment
 import StageData
-import UserInterface
+import qualified UserInterface as UI
 
-loadGame :: String -> Maybe (World, Map Name [Event])
+loadGame :: String -> IO (Maybe (World, Map.Map Name [Event]))
 loadGame = undefined -- Call the compiler to do this
 
 
-perform :: (Map Name [Event]) -> World -> IO ()
-perform events world = do event <- getEventInput currentEvents
-                          runResult <- runEvent world event
-                          case runResult of
+perform :: (Map.Map Name [Event]) -> World -> IO ()
+perform events world = do eventChoice <- getEvent events world
+                          case eventChoice of
                             Nothing -> gameOver
-                            Just world' -> perform world' events
-  where currentEvents :: Set Event
-        currentEvents = Map.keys (Map.filter (\es -> hasRunnable es) events)
+                            Just event -> do runResult <- runEvent world event
+                                             case runResult of
+                                               Nothing -> gameOver
+                                               Just world' -> perform events world'
+  where
+    gameOver = return () -- figure out something better to do here
+
+-- Describe the world to the player and get an event choice
+getEvent :: (Map.Map Name [Event]) -> World -> IO (Maybe Event)
+getEvent events world =
+  do UI.outputWorld world
+     choice <- UI.getEventInput currentEvents
+     case choice of
+       Nothing -> return Nothing
+       Just n  -> return (Just (head (getRunnable (fromJust (Map.lookup n events)))))
+  where currentEvents :: Set.Set Name
+        currentEvents = Map.keysSet (Map.filter (\es -> hasRunnable es) events)
+        getRunnable :: [Event] -> [Event]
+        getRunnable es = foldr (\e l -> if (shouldRun e) world then e : l else l ) [] es
         hasRunnable :: [Event] -> Bool
-        hasRunnable = foldr (\e b -> b || (shouldRun e) world) False
+        hasRunnable es = null (getRunnable es)
 
--- Describe the world then prompt for an event
-getEvent :: (Map Name [Event]) -> World -> IO Event
-getEvent events world = do outputWorld world
-                           getEventInput 
 
--- Execute the event and describe what happened
+-- Describe the event and then execute it
 runEvent :: World -> Event -> IO (Maybe World)
-runEvent = undefined
+runEvent world event = do UI.outputEvent world event
+                          return ((updateWorld event) world)
 
 
 main :: IO ()
 main = do args <- getArgs
           case args of
-          []         -> printUsage
-          [gameFile] -> do fileContents <- readFile gameFile
-                           game <- loadGame fileContents
-                           case g of
-                             Nothing              -> printError gameFile
-                             Just (world, events) -> perform events world
+            []         -> printUsage
+            [gameFile] -> do fileContents <- readFile gameFile
+                             game <- loadGame fileContents
+                             case game of
+                               Nothing              -> printError gameFile
+                               Just (world, events) -> perform events world
   where printUsage = putStrLn "Usage: StageEngine gameFile"
         printError filename = putStrLn ("Error: Unable to load game from " ++ filename)
 
