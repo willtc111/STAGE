27b71b1ea5f8ba3c4a11bf5acb6b402b123c6868
William Thomas Carver <wtc9653@cs.rit.edu>
2018-12-14 22:17:25 -0500

    Added the rest of the pretty printers for StageParserTests.

--- StageParserTests.hs
+++ StageParserTests.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module StageParserTests where
 
 import StageParser
@@ -32,6 +34,20 @@ listD d elems   = aux elems
   where
     aux [e]    = text "and" <+> d e
     aux (e:es) = d e <> text "," <+> aux es
+
+conditionD :: SCD.Condition -> Doc
+conditionD (SCD.LocationCondition pred)   = text "the player"
+                                            <+> predD pred
+conditionD (SCD.PlayerCondition pred)     = text "the current location"
+                                            <+> predD pred
+conditionD (SCD.OrCondition cond1 cond2)  = text "either"
+                                            <+> conditionD cond1
+                                            <+> text "or"
+                                            <+> conditionD cond2
+conditionD (SCD.AndCondition cond1 cond2) = text "both"
+                                            <+> conditionD cond1
+                                            <+> text "and"
+                                            <+> conditionD cond2
 
 predD :: SCD.Pred -> Doc
 predD p = case p of
@@ -77,7 +93,10 @@ cmpd SCD.GeCmp = text ">="
 
 modD :: SCD.Mod -> Doc
 modD (SCD.DoNothingMod)         = text "doing nothing"
-modD (SCD.SetMod id expr)       = undefined -- TODO once exprD is added
+modD (SCD.SetMod id expr)       = text "setting its"
+                                  <+> idD id
+                                  <+> text "to"
+                                  <+> exprD expr
 modD (SCD.GiveMod id)           = text "giving it" <+> idD id
 modD (SCD.TakeMod id)           = text "taking" <+> idD id <+> text "from it"
 modD (SCD.IfMod pred modT modF) = undefined -- TODO once predD is added
@@ -86,6 +105,23 @@ modD (SCD.AndMod mod1 mod2)     = text "first" <+> modD mod1 <+> text "and then"
 
 exprD :: SCD.Expr -> Doc
 exprD = undefined
+
+thingDescD :: SCD.ThingDesc -> Doc
+thingDescD = undefined
+
+actionDescD :: SCD.ActionDesc -> Doc
+actionDescD = undefined
+
+classDeclD :: SCD.ClassDecl -> Doc
+classDeclD cls = text "A"
+                 <+> idD (SCD.classId cls)
+                 <+> if null (SCD.classStats cls)
+                       then empty
+                       else text "has"
+                            <+> statsD (SCD.classStats cls)
+                            <+> text "and"
+                <+> text "is described by"
+                <+> thingDescD (SCD.classDesc cls)
 
 thingThingDeclD :: SCD.ThingDecl -> Doc
 thingThingDeclD thing = thingDeclD "Thing" thing
@@ -104,10 +140,68 @@ thingDeclD kind thing = text kind
                               else text "with" <+> statsD (SCD.stats thing)
                         <+> if null (SCD.contents thing)
                               then empty
-                              else text "that contains" <+> aux (SCD.contents thing)
+                              else text "that contains" <+> thingsD (SCD.contents thing)
+
+
+thingsD :: [SD.Id] -> Doc
+thingsD [] = empty
+thingsD [id] = text "thing" <+> idD id
+thingsD ids = text "things" <+> listD idD ids
+
+
+actionDeclD :: SCD.ActionDecl -> Doc
+actionDeclD SCD.ActionDecl{..} = 
+  text "Action"
+  <+> nameD actionName
+  <+> text "is available when"
+  <+> conditionD condition
+  <> text ", modifies the player by"
+  <+> modD modifyPlayer
+  <> text ", modifies the current location by"
+  <+> modD modifyCurrentLocation
+  <+> case newLocation of
+        Just id -> text "before setting the current location to" <+> idD id
+        Nothing -> empty
+  <> text ", and is described by"
+  <+> actionDescD actionDesc
+  <> text "."
+-- actionName condition actionDesc
+actionDeclD SCD.GameEndDecl{..} = 
+  text "Action"
+  <+> nameD actionName
+  <+> text "is available when"
+  <+> conditionD condition
+  <> text ", ends the game, and is described by"
+  <+> actionDescD actionDesc
+
+
+declD :: SCD.Decl -> Doc
+declD (SCD.ClassDecl' cDecl)  = classDeclD cDecl
+declD (SCD.ThingDecl' tDecl)  = thingThingDeclD tDecl
+declD (SCD.ActionDecl' aDecl) = actionDeclD aDecl
+
+declsD :: SCD.Decls -> Doc
+declsD decls = classDeclsD $$ thingDeclsD $$ actionDeclsD
   where
-    aux [] = empty
-    aux [id] = text "thing" <+> idD id
-    aux ids = text "things" <+> listD idD ids
+    classDeclsD = fsep $ fmap classDeclD (SCD.classDecls decls)
+    thingDeclsD = fsep $ fmap thingThingDeclD (SCD.thingDecls decls)
+    actionDeclsD = fsep $ fmap actionDeclD (SCD.actionDecls decls)
 
-
+playerDeclD :: SCD.PlayerDecl -> Doc
+playerDeclD SCD.PlayerDecl{..} =
+  text "The player"
+  <+> if null playerStats
+        then empty
+        else text "has"
+             <+> statsD playerStats
+             <+> text "and"
+  <+> if null playerThings
+        then empty
+        else text "has"
+             <+> thingsD playerThings
+             <+> text "and"
+  <+> text "starts in"
+  <+> idD playerStart
+  <+> text "and is described by"
+  <+> thingDescD playerDesc
+  <+> text "."
